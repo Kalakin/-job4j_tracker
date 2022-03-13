87d6a6a14edf410a86b86101b2f98fe1cc912762
User <kirill.kalakin@gmail.com>
2022-03-13 19:57:11 +0300

    1.4. Вызов метода с возвращаемым типом. [#167387]

--- src/main/java/oop/DummyDic.java
+++ src/main/java/oop/DummyDic.java
@@ -1,15 +1,13 @@
 package oop;
 
 public class DummyDic {
-    public String engToRus() {
-        String eng = "Translate";
-        return eng;
+    public String engToRus(String eng) {
+        return "Неизвестное слово: " + eng;
     }
 
     public static void main(String[] args) {
         DummyDic translate = new DummyDic();
         String result = translate.engToRus();
         System.out.println("Неизвестное слово " + result);
-
     }
 }
