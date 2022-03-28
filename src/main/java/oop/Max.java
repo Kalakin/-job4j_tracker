8816ff2b6a34235996dbc7d6e1bf4f06d684edee
User <kirill.kalakin@gmail.com>
2022-03-28 13:42:02 +0300

    3. Перегрузить метод max для трех чисел [#122644]

---
+++ src/main/java/oop/Max.java
@@ -0,0 +1,20 @@
+package oop;
+
+public class Max {
+
+    public int max(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public int max(int a, int b, int c) {
+        return max(a, max(b, c));
+    }
+
+    public int max(int a, int b, int c, int d) {
+        return max(max(a, b), max(c, d));
+    }
+}
