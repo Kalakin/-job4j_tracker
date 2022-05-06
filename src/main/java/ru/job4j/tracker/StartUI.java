71d0cb05a65215e857ecc3fa759f349b58d47bf2
User <kirill.kalakin@gmail.com>
2022-05-06 13:32:09 +0300

    7. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,11 @@
+package ru.job4j.tracker;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    StartUI Item = new StartUI();
+    Item.getCreated();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+    String created = created.format(formatter);
+
+}
---
+++ src/main/java/ru/job4j/tracker/UsagePackageTime.java
@@ -0,0 +1,10 @@
+package ru.job4j.tracker;
+
+import java.time.LocalTime;
+
+public class UsagePackageTime {
+    public static void main(String[] args) {
+        LocalTime currentTime = LocalTime.now();
+        System.out.println("Текущее время: " + currentTime);
+    }
+}
