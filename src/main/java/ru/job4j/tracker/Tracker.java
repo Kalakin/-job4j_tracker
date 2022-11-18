9d1a0a2fb80d8bea56787a9f0dcd06fb4971cb0f
User <kirill.kalakin@gmail.com>
2022-11-18 19:05:30 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -40,5 +40,20 @@ public class Tracker {
         }
         return rsl;
     }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+      int index indexOf(id);
+    }
 }
 
