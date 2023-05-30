24afd3578bf55ea53f1c7303f9483714df68a909
User <kirill.kalakin@gmail.com>
2023-05-30 14:05:58 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -9,5 +9,9 @@ public class StartUI {
         item.getCreated();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         System.out.println("Текущие дата и время: " + item.getCreated().format(formatter));
+
+        Item item2 = new Item("Item2");
+        item2.toString();
+        System.out.println("Время и дата:" + item2.toString());
     }
 }
