8189d321e34ace641eb87c419f160536c08678ed
User <kirill.kalakin@gmail.com>
2023-06-05 15:47:53 +0300

    4.2. Интерфейс Input [#181092 #420584]

--- src/main/java/ru/job4j/oop/polymorph/Bus.java
+++ src/main/java/ru/job4j/oop/polymorph/Bus.java
@@ -4,21 +4,23 @@ public class Bus implements Transport {
 
     @Override
     public void going() {
-
+        System.out.println("Ехать");
     }
 
     @Override
     public void pasangers(int peoples) {
-
+        System.out.println("Пассажиры");
     }
 
     @Override
     public void refuel(int gallons) {
-        System.out.println("Цена за топливо равна: ");
+        System.out.println("Цена за топливо равна: " + gallons * 30 + "рублей.");
     }
 
     public static void main(String[] args) {
         Transport bus1 = new Bus();
         bus1.refuel(10);
+        bus1.going();
+        bus1.pasangers(6);
     }
 }
