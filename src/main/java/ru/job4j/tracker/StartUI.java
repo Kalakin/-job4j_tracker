7f00ff6f416744e17782e9effbe9ddcfd788e61b
User <kirill.kalakin@gmail.com>
2023-10-28 14:11:13 +0300

    6.1. Разрыв зависимости StartUI от Scanner. [#181778 #421776]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -13,7 +13,6 @@ public class StartUI {
             int select = input.askInt("Select: ");
             if (select == 0) {
                 System.out.println("=== Create a new Item ===");
-                System.out.print("Enter name: ");
                 String name = input.askStr("Enter name: ");
                 Item item = new Item(name);
                 tracker.add(item);
@@ -30,9 +29,7 @@ public class StartUI {
                 }
             } else if (select == 2) {
                 System.out.println("===Edit item===");
-                System.out.print("Enter id: ");
                 int id = input.askInt("Enter id: ");
-                System.out.print("Enter name: ");
                 String name = input.askStr("Enter name: ");
                 Item item = new Item(name);
                 if (tracker.replace(id, item)) {
@@ -42,7 +39,6 @@ public class StartUI {
                 }
             } else if (select == 3) {
                 System.out.println("===Delete item===");
-                System.out.print("Enter id: ");
                 int id = input.askInt("Enter id: ");
                 if (tracker.delete(id)) {
                     System.out.println("Заявка удалена успешно.");
@@ -51,7 +47,6 @@ public class StartUI {
                 }
             } else if (select == 4) {
                 System.out.println("===Find item by id===");
-                System.out.print("Enter id: ");
                 int id = input.askInt("Enter id: ");
                 Item item = tracker.findById(id);
                 if (item != null) {
@@ -61,7 +56,6 @@ public class StartUI {
                 }
             }  else if (select == 5) {
                 System.out.println("=== Find items by name ===");
-                System.out.print("Enter name: ");
                 String name = input.askStr("Enter name: ");
                 Item[] items = tracker.findByName(name);
                 if (items.length > 0) {
