a98db371b1987b2cb58760c23f7fa00662cadc56
User <kirill.kalakin@gmail.com>
2023-11-09 16:42:13 +0300

    2.1. Тест на ValidateInput [#311887]

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.tracker;
+
+import org.assertj.core.api.AbstractIntegerAssert;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ValidateInputTest {
+
+    @Test
+    void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new MockInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    void whenMultiplyNumberInput() {
+        Output out = new StubOutput();
+        Input in = new MockInput(
+                new String[] {"4", "10"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(4);
+        int selected2 = input.askInt("Enter menu:");
+        assertThat(selected2).isEqualTo(10);
+    }
+
+
+    @Test
+    void whenNegativeNumberInput() {
+        Output out = new StubOutput();
+        Input in = new MockInput(
+                new String[] {"-1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(-1);
+    }
+}
