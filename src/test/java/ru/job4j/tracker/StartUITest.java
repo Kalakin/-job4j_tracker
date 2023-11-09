c283520e076c5168933231b5f60e6697acb66b22
User <kirill.kalakin@gmail.com>
2023-11-09 15:27:55 +0300

    1.1. Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -163,4 +163,26 @@ class StartUITest {
                         + "=== Завершение программы ===" + ln
         );
     }
+
+    @Test
+    void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"10" , "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString()).isEqualTo(
+                "Menu:" + ln
+                        + "0. Exit Program" + ln
+                        + "Неверный ввод, вы можете выбрать: 0....0" + ln
+                        + "Menu:" + ln
+                        + "0. Exit Program" + ln
+                        + "=== Завершение программы ===" + ln
+        );
+    }
 }
