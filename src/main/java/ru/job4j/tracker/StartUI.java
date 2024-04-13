7402b803823c7055be48528f28c19ddfcab8a9e6
Кирилл <kirill.kalakin@gmail.com>
2024-04-13 16:21:15 +0300

    Именование классов реализующих интерфейс [#505124]
    Signed-off-by: Кирилл <kirill.kalakin@gmail.com>

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,7 +1,6 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import ru.job4j.tracker.action.*;
 
 public class StartUI {
     private final Output out;
--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,9 +1,9 @@
 package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
+import ru.job4j.tracker.action.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatNullPointerException;
 
 class StartUITest {
     @Test
